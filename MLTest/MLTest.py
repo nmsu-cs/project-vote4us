d3b331c20702577efdaa9715b911e205203ac3a6
LTiede4 <lucastiede@icloud.com>
2024-04-11 19:58:04 -0600

    new branch for virtual environment testing

--- MLTest/MLTest.py
+++ MLTest/MLTest.py
@@ -8,12 +8,13 @@ from cvlib.object_detection import draw_bbox
 # turn off floating-point round-off errors
 #TF_ENABLE_ONEDNN_OPTS = 0
 DEBUG = 0   # 0 is comments off, 1 is comments on
+CAPTURE = 1   # controls capture device
 
 
 
 # This is for object detection in video capture
 def runObjectDetection() :
-    video = cv2.VideoCapture(0)   # the argument modifies the capture device, (0 = my webcam)
+    video = cv2.VideoCapture(CAPTURE)   # the argument modifies the capture device, (0 = my webcam)
     # run until space key is pressed
     while True:
         ret, frame  = video.read()
@@ -31,11 +32,18 @@ def runObjectDetection() :
     
         output_image = draw_bbox(frame, bbox, label, conf)
         cv2.imshow("Object Detection", frame)
-        if cv2.waitKey(1) & 0xFF == ord(" "):
+        if cv2.waitKey(1) & 0xFF == ord(" ") :
             os.remove('frame.jpg')
             cv2.destroyAllWindows()
             break
 
+        # # TESTING ALLOWING CLICKING THE 'X' TO CLOSE WINDOW
+        # # press space or 'X' button to close window
+        # if (cv2.waitKey(1) & 0xFF == ord(" ")) | (cv2.waitKey(1) & cv2.getWindowProperty('Object Detection', 0) < 0) :
+        #     os.remove('frame.jpg')
+        #     cv2.destroyAllWindows()
+        #     break
+    
 
 
 # This is for face detection in an image
@@ -67,7 +75,7 @@ def runObjectDetection() :
 
 # This is for face detection in an image
 def runFaceDetection() :
-    video = cv2.VideoCapture(0)   # the argument modifies the capture device, (0 = my webcam)
+    video = cv2.VideoCapture(CAPTURE)   # the argument modifies the capture device, (0 = my webcam)
     while True:
         ret, frame  = video.read()
         cv2.imwrite("frame.jpg", frame)
@@ -81,10 +89,12 @@ def runFaceDetection() :
             (endX,endY) = face[2],face[3]
             cv2.rectangle(frame, (startX,startY), (endX,endY), (0,255,0), 2)
         cv2.imshow("Face Detection", frame)
-        if cv2.waitKey(1) & 0xFF == ord(" "):
-            os.remove('frame.jpg')
+        # press space to close window
+        if cv2.waitKey(1) & 0xFF == ord(" ") :
             cv2.destroyAllWindows()
+            os.remove('frame.jpg')
             break
+    
 
 
 #print("done")
